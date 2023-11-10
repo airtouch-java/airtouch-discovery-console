a3acea146c2eed4debd79206d4cdeca51168bbb6
Net Wolf UK <netwolfuk@gmail.com>
2023-11-11 07:54:48 +1300

    Add exception logger and handler for whole app

--- src/main/java/airtouch/console/App.java
+++ src/main/java/airtouch/console/App.java
@@ -1,15 +1,22 @@
 package airtouch.console;
 
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import airtouch.console.app.AirtouchConsole;
 
 public class App {
 
+	private static final Logger log = LoggerFactory.getLogger(App.class);
 
 
-	public static void main(String[] args) throws InterruptedException, IOException {
+	public static void main(String[] args) throws Exception {
 		AirtouchConsole console = new AirtouchConsole();
-		console.begin();
+		try {
+			console.begin();
+		} catch (Exception e) {
+			log.error("Exception in App. Exiting....", e);
+			throw e;
+		}
 	}
 }
